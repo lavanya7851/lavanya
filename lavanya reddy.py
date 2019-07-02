6c968dd041099fec0f21c43612e23ef89d9afaa9
lavanya7851 <52449541+lavanya7851@users.noreply.github.com>
2019-07-02 15:43:17 +0530

    Update lavanya reddy.py

--- lavanya reddy.py
+++ lavanya reddy.py
@@ -1,7 +1,7 @@
-num=int(input())
-if num > 0:
-print("Positive")
-elif num<0:
-print("Negative")
+a=int(input())
+if a>0:
+  print('Positive')
+elif a<0:
+  print('Negative')
 else:
-print("Zero")
+  print('Zero')

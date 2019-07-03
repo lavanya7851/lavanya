e62dbd7e7ca7d930ec87317b4383c4029a0c0699
lavanya7851 <52449541+lavanya7851@users.noreply.github.com>
2019-07-03 10:41:57 +0530

    Create lovely.py

---
+++ lovely.py
@@ -0,0 +1,8 @@
+     s=input()
+     s=s.lower()
+     if s in['a','e','i','o','u']:
+     print("vowel")
+     elif ord(c) in range(97,123)
+     print("consonant")
+     else:
+     print("Invalid")

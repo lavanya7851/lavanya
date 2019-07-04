0b88577c8a80021f7f479455cd477130e1e77cb5
lavanya7851 <52449541+lavanya7851@users.noreply.github.com>
2019-07-04 11:37:28 +0530

    Rename lovely.py to reddy.py

---
+++ reddy.py
@@ -0,0 +1,10 @@
+n=int(input("enter a number:"))
+x=n
+rev=0
+while x!=0
+rev=(rev*10)+(x%10)
+x=x // 10
+if num% == rev
+print("number is a palindrome:")
+else:
+print("number is not a palindrome:")
